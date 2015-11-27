42f8789c2c852deab93f8bfb4ee5071474308050
Navo <mail@stanislav-hetzel.de>
2015-11-27 20:45:11 +0100

    Fixed repository & removed unused imports.

--- src/main/java/de/howaner/FakeMobs/util/MobShop.java
+++ src/main/java/de/howaner/FakeMobs/util/MobShop.java
@@ -4,11 +4,7 @@ import de.howaner.FakeMobs.merchant.Merchant;
 import de.howaner.FakeMobs.merchant.MerchantOffer;
 import java.util.ArrayList;
 import java.util.List;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class MobShop {
 	private List<MerchantOffer> items = new ArrayList<MerchantOffer>();
